1341e620e5925c548324a243573ba48a29b5b950
Anthony K GROSS <anthony.k.gross@gmail.com>
2017-09-25 17:23:29 +1000

    Update Client and fix issues

--- src/src/KkuetNet/SharindataBundle/Security/Core/Authentication/Provider/Provider.php
+++ src/src/KkuetNet/SharindataBundle/Security/Core/Authentication/Provider/Provider.php
@@ -15,4 +15,17 @@ class Provider extends \Escape\WSSEAuthenticationBundle\Security\Core\Authentica
     {
         return hash_hmac('sha512', $user->getApiSecret() , $user->getHashKey());
     }
+
+    protected function validateDigest($digest, $nonce, $created, $secret, $salt)
+    {
+        try {
+            parent::validateDigest($digest, $nonce, $created, $secret, $salt);
+        }
+        catch (NonceExpiredException $e) {
+
+        }
+        $nonce = base64_decode($nonce);
+        $passwordDigest = base64_encode(sha1($nonce . $created .$secret, true));
+        return $digest === $passwordDigest;
+    }
 }
